3176f9d0a7c5b05a695ed2d0ebbb10ecabd3eca3
OrbenMichele <micheleorben@gmail.com>
2019-04-23 18:01:32 -0300

    added visits, updated owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/PetType.java
@@ -1,14 +1,35 @@
 package om.springframework.sfgpetclinic.model;
 
+import java.time.LocalDate;
+
 public class PetType extends BaseEntity {
 
-    private String name;
 
-    public String getName() {
-        return name;
+    private PetType petType;
+    private Owner owner;
+    private LocalDate localDate;
+
+    public PetType getPetType() {
+        return petType;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
     }
 }
