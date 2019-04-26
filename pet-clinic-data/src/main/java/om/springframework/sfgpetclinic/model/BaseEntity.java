82c6d972d28ed6af3f8c255d6bdb51ef9d84b4ed
OrbenMichele <micheleorben@gmail.com>
2019-04-26 16:13:38 -0300

    Updated base entiry with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package om.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+//diz para o JPA que é uma classe base, there is no need to map this class to the database
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
