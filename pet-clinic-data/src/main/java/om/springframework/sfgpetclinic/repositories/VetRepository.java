ce40b579ef1bdfcbb31d1188d3e5b61a53b8b94a
OrbenMichele <micheleorben@gmail.com>
2019-04-26 17:51:10 -0300

    Added spring data jpa for vets. Closes #41

--- pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/repositories/VetRepository.java
@@ -3,5 +3,12 @@ package om.springframework.sfgpetclinic.repositories;
 import om.springframework.sfgpetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface VetRepository extends CrudRepository<Vet, Long> {
+
+    Vet findByLastName(String lastName);
+    Vet findByFirstName(String firstName);
+    Set<Vet> findAll();
+
 }
--- pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/services/VetService.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface VetService extends CrudService<Vet, Long> {
 
+    //Vet findByLastName(String lastName);
 
 }
