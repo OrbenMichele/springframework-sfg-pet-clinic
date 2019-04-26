ef8340e88dbd0fe4d6849af3f5a5fccfb5bfb724
OrbenMichele <micheleorben@gmail.com>
2019-04-26 16:59:38 -0300

    Updated Visit to use JPA entities. Closes #38

--- pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package om.springframework.sfgpetclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pet extends BaseEntity {
@@ -22,6 +21,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
 
     public String getName() {
--- pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/model/Visit.java
@@ -1,13 +1,20 @@
 package om.springframework.sfgpetclinic.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 
-//@Entity
+@Entity
+@Table(name = "visits")
 public class Visit extends  BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
 
