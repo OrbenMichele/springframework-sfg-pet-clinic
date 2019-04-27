9b851e12d785bcf5d9cb289e4a2b5379dc5aaa0d
OrbenMichele <micheleorben@gmail.com>
2019-04-26 21:23:35 -0300

    Updating #46

--- pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/services/map/VisitServiceMap.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+public class VisitServiceMap extends AbstractServiceMap<Visit, Long> implements VisitService {
 
     private final VisitRepository visitRepository;
 
@@ -27,13 +27,18 @@ public class VisitServiceMap extends AbstractMapService<Visit, Long> implements 
     }
 
     @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null){
+            throw new RuntimeException("Invalid Visit.");
+        }
+
+        return super.save(visit);
     }
 
     @Override
-    public Visit save(Visit object) {
-        return super.save(object);
+    public void deleteById(Long id) {
+        super.deleteById(id);
     }
 
     @Override
