c8b9b81cf870934e361ea467bcafadb6aa3334e4
OrbenMichele <micheleorben@gmail.com>
2019-04-26 21:09:54 -0300

    Closes #44 Closes #46 Closes #47

---
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package om.springframework.sfgpetclinic.services;
+
+import om.springframework.sfgpetclinic.model.Visit;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface VisitService extends CrudService<Visit, Long> {
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/om/springframework/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,43 @@
+package om.springframework.sfgpetclinic.services.map;
+
+import om.springframework.sfgpetclinic.model.Visit;
+import om.springframework.sfgpetclinic.repositories.VisitRepository;
+import om.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitServiceMap(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+}
